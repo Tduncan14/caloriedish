e9cc088e098f3049205f1bd6f61c2e5d4c74041e
Tre'Shawn <nbatre1@yahoo.com>
2019-06-23 13:41:36 -0500

    keep moving forward

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const itemCtrl = (function(){
       this.calories = calories;
   }
 
+
+
+
   // Data Structure / State
    const data = {
        items:[{id:0,name:'Steak Dinner',calories:1200},
@@ -19,27 +22,128 @@ const itemCtrl = (function(){
        totalCalories:0
 
    }
-
+   //public method
    return{
+       getData:function(){
+            return data.items
+       },
+
        logData: function(){
          return data
        }
    }
 })();
 
+
+
+
+
+
 //ui controller
 
-const UIctr = (function(){
+const UIctrl = (function(){
+
+    const UISelectors={
+        itemList:'#item-list',
+        addBtn:'.add-btn',
+        itemNameInput:'#item-name',
+        itemCaloriesInput:'#item-calories'
+      }
+
+    //public method
+    return{
+
+        
+
+       populateList:function(items){
+
+        let html = ``;
+
+        items.forEach(item =>{
+         html += ` <li class="collection-item" id="${item.id}">
+         <strong>${item.name}:</strong><em>${item.calories}</em>
+         <a href="#" class="secondary-content"><i class="far fa-edit fa fa-pencil"></i></a>
+</li>`})
+        // insert list items
+        document.querySelector(UISelectors.itemList).innerHTML = html;
+
+       },
+        getItemInput:function(e){
+            return{
+                name:document.querySelector(UISelectors.itemNameInput.value),
+                calories:document.querySelector(UISelectors.itemCaloriesInput.value)
+            }
+
+        }
+       ,
+       getSelectors:function(){
+        return UISelectors;
+    }
+    }
+    
 
 })()
 
 
 
 
-// app controller
-const App = (function(itemCtrl,UIctr){
 
+
+
+
+
+// app controller
+const App = (function(itemCtrl,UIctrl){
+
+// load event listeners
+const loadEventListeners =function(){
+   const UISelectors = UIctrl.getSelectors();
+
+
+   // Add Item event
+   document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+
+}
+
+
+// Add item submit
+
+  const itemAddSubmit = function(e){
+
+    // Get form input from UI controller
+    const input = UIctrl.getItemInput();
+
+      e.preventDefault();
+
+      
+      console.log('add');
+  }
 
     // console.log(itemCtrl.logData())
 
-})(itemCtrl,UIctr);
+
+  //Public method
+    return{
+        init:function(){
+            console.log("Starting app");
+
+            //fetches the items form the data structure
+            const items = itemCtrl.getData();
+            // console.log(items,"items");
+
+            //populate items list
+            UIctrl.populateList(items);
+
+           //load event listeners
+           loadEventListeners();
+
+        }
+    }
+
+})(itemCtrl,UIctrl);
+
+
+
+// Starting 
+
+App.init();
